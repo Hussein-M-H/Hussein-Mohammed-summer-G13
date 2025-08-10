ccef098992d5d46061a8dff57b0b377e8e526760
NouranFayez <nouran_ahmed269@hotmail.com>
2025-08-10 10:31:22 +0300

    session 11 part 1

---
+++ session11/js/script.js
@@ -0,0 +1,116 @@
+
+
+// var , let , const
+
+var firstName = "Nouran"
+var age = 31
+var lastName = "Fayez"
+
+var fullName = firstName+" "+lastName
+
+// template string
+var fullName = `${firstName}
+ ${lastName}`
+
+ var age = "bvjb"
+
+// console.log(fullName)
+// console.log(age)
+
+var isGraduated = true
+
+// console.log(isGraduated)
+
+var g
+
+// console.log(g)
+
+var a = null
+
+// console.log(a)
+
+var h = ""
+
+// var p = prompt("enter your name")
+// console.log(p)
+
+
+// mathematical operations = (+ - / * ** % ++ --)
+
+
+var a = 30
+var b = 10
+
+var sum = a+b
+// console.log(sum)
+
+// ++ increment 
+
+var h = 10
+
+// console.log(h++) // 10
+// console.log(++h) // 12
+// console.log(h) // 12
+
+// -- decrement
+
+// console.log(h--)
+// console.log(--h)
+// console.log(h)
+
+
+var num = 5
+
+num = num + 5
+
+// console.log(num+=20)
+
+// console.log(num/=3)
+
+
+var k = 10
+var n = "str"
+
+var m = k / n 
+// console.log(m) // NaN ==> Not a Number
+
+
+
+// comparison operations (< , <= , > , >= , == , === , != , !==) ==> return boolean
+var num = 10
+var num2 = 20
+var num3 = 10
+
+// console.log(num < num2)
+// console.log(num <= num3)
+
+var g = 10
+var h = "10"
+
+// console.log(g == h) // value
+// console.log(g === h) // value , data type
+
+
+// console.log(g != h) // value
+// console.log(g !== h) // value , data type
+
+
+// console.log(typeof(g))
+// console.log(typeof h)
+
+//  logical operator (&& || !)
+
+var num = 20
+var str = "text"
+var firstName = "Marwa"
+
+// console.log(num == 20)
+// console.log(firstName == "Marwa")
+// console.log(typeof str == 'string')
+
+// console.log(num == 40 && firstName == "Marwa" && typeof str == "string")
+// console.log(num == 40 || firstName == "Nouran" || typeof str == "string")
+
+// console.log(typeof num )
+// console.log(!typeof num == 'Number') 
+console.log(!firstName == 'Marwa')
